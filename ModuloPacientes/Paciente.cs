2ec68feb2f7a4feaba9fb025b1113b1c75f572a1
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-05-08 13:48:06 -0300

    add clast pacientes, test

---
+++ ModuloPacientes/Paciente.cs
@@ -0,0 +1,22 @@
+
+
+using ControleDeMedicamentos.ConsoleApp.Compartilhado;
+
+namespace ControleDeMedicamentos.ConsoleApp.ModuloPacientes;
+
+public class Paciente : EntidadeBase
+{
+    public Paciente()
+    {
+    }
+
+    public override void AtualizarDados(EntidadeBase entidadeAtualizada)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override List<string> Validar()
+    {
+        throw new NotImplementedException();
+    }
+}
